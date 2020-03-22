16fb603ec362f97b05b4ca7b75352bd81205c118
snn <snn@snndembp.lan>
2020-03-22 23:04:58 +0800

    系列化注意事项

--- src/com/nan/serializable/SerializeDemo.java
+++ src/com/nan/serializable/SerializeDemo.java
@@ -8,6 +8,15 @@ import java.io.ObjectOutputStream;
 
 public class SerializeDemo {
 
+	/**
+	 * /**
+	 * 请注意，一个类的对象要想序列化成功，必须满足两个条件：
+	 * (1)该类必须实现 java.io.Serializable 接口。
+	 * (2)该类的所有属性必须是可序列化的。如果有一个属性不是可序列化的，则该属性必须注明是短暂的transient。无赋值时变量的值被设为初始值，如 int 型的是 0，对象型的是 null
+	 * (3)静态变量属于类的状态，因此 序列化并不保存静态变量
+	 * 如果你想知道一个 Java 标准类是否是可序列化的，请查看该类的文档。检验一个类的实例是否能序列化十分简单， 只需要查看该类有没有实现 java.io.Serializable接口。
+	 * 
+	 */
 	public static void main(String[] args) {
 		serialize();
 		deserialize();
@@ -29,7 +38,6 @@ public class SerializeDemo {
 		} catch (IOException i) {
 			i.printStackTrace();
 		}
-		System.err.println(User.staticVar);
 	}
 	
 	
@@ -50,9 +58,6 @@ public class SerializeDemo {
 			c.printStackTrace();
 			return;
 		}
-		System.out.println(user.toString());
-		System.out.println(user.staticVar);
-		System.out.println(User.staticVar);
 	}
 
 
